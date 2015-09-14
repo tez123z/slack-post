a77630521a511eed999abaafca22ec4a5d1aba53
Tom Vestal Evans <tom@sovi.fm>
2015-09-14 14:39:22 -0700

    added fields for latest api and ability to add custom username on posts

--- lib/slack/post.rb
+++ lib/slack/post.rb
@@ -17,7 +17,9 @@ module Slack
 				channel: chan || config[:channel],
 				text: message
 			}
-			if config[:username]
+			if opts.key?(:username)
+				pkt[:username] = opts[:username]
+			elsif config[:username]
 				pkt[:username] = config[:username]
 			end
 			if opts.key?(:icon_url) || config.key?(:icon_url)
@@ -89,7 +91,7 @@ module Slack
 		end
 
 		KnownConfigParams = [:webhook_url, :username, :channel, :subdomain, :token, :icon_url, :icon_emoji].freeze
-		AttachmentParams = [:fallback, :text, :pretext, :color, :fields, :image_url].freeze
+		AttachmentParams = [:fallback, :text, :pretext, :author_name, :author_link, :author_icon, :title, :title_link, :color, :fields, :image_url].freeze
 		FieldParams = [:title, :value, :short].freeze
 
 		def self.prune(opts, allowed_elements = KnownConfigParams)
