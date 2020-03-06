7f0945d325a77dd4d82bf3c8a3983a36e1e5c0f6
tez123z <tom@sovi.fm>
2020-03-06 14:15:18 -0800

    Update post.rb

--- lib/slack/post.rb
+++ lib/slack/post.rb
@@ -34,7 +34,7 @@ module Slack
 			end
 			uri = URI.parse(post_url)
 			http = Net::HTTP.new(uri.host, uri.port)
-			http.use_ssl = false
+			http.use_ssl = true
 			http.ssl_version = :TLSv1
 			http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 			req = Net::HTTP::Post.new(uri.request_uri)
